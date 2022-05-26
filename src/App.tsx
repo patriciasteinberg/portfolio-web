28e9cd88adedbd95501e8715e5fab113c3a97d24
Patricia Steinberg <50302759+patriciasteinberg@users.noreply.github.com>
2022-05-26 16:15:30 -0400

    feat: change head tags and favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
 import NotFound from "./components/NotFound/NotFound";
 import "./shared/tailwind.scss";
+import Portfolio from "./components/Portfolio/Portfolio";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
-        <Route path="/" element={<p>Portfolio</p>} />
+        <Route path="/" element={<Portfolio />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 };
